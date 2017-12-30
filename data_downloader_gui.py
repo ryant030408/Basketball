c4322c0303b065c7bb78fa482554036666e8c640
Rtay585 <ryant030408@gmail.com>
2017-12-30 00:28:53 -0500

    created frame builder

--- data_downloader_gui.py
+++ data_downloader_gui.py
@@ -4,6 +4,36 @@ from tkinter import *
 import tkinter as tk
 import tkinter.ttk as ttk
 import graph_gui_functions as gf
+from pathlib import Path
+
+# decides which functon to execute
+def download_finder(stat):
+
+
+# fills frame with data about files
+def frame_filler(self, stat):
+
+    # create filename
+    file_name = "/" + stat + "/" + stat + "_for_" + self.yearVar.get()
+    file = Path(file_name)
+
+    # frame of widget
+    frame = tk.Frame(self, borderwidth=1, relief="solid")
+    label = tk.Label(frame, text=stat)
+    label.grid(row=0, column=0)
+
+    # check if stat is downloaded
+    if file.is_file():
+        # text if already downloaded
+        downloaded = Label(frame, text="COMPLETE")
+        downloaded.pack()
+    else:
+        # get button
+        button = tk.Button(frame, text='GET', pady=25)
+        button.grid(row=1, column=0)
+
+    return frame
+
 
 # gui
 class App(tk.Tk):
@@ -24,6 +54,7 @@ class App(tk.Tk):
         # year drop down
         self. yearVar = StringVar()
         self.year_combobox = ttk.Combobox(self.year_frame, values=gf.year_creator(), textvariable=self.yearVar)
+        self.year_combobox.current(1)
         self.year_combobox.pack()
 
         # add year drop down to gui
@@ -31,6 +62,7 @@ class App(tk.Tk):
 
         #------------------------------------
 
+        frame_filler(self, 'play_by_play').grid(row=1, column=0)
 
 
 

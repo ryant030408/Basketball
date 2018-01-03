bb2319a91d19dc837edd925ee3a8cba095cdd791
Rtay585 <ryant030408@gmail.com>
2018-01-02 21:31:05 -0500

    small edits and changes

--- constants.py
+++ constants.py
@@ -2,3 +2,16 @@ PLAYER_GAME_LOG_STAT_LIST = ['MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3
                                  'OREB', 'DREB','REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'PLUS_MINUS']
 TEAM_GAME_LOG_STAT_LIST = ['FGM', 'FGA','FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB',
                                'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
+DOWNLOAD_STATS_PLAYER = ['PlayerCareer', 'PlayerClutchSplits', 'PlayerDefenseTracking', 'PlayerGameLogs',
+                         'PlayerGeneralSplits', 'PlayerInGameSplits', 'PlayerLastNGamesSplits', 'PlayerList',
+                         'PlayerOpponentSplits', 'PlayerPassTracking', 'PlayerPerformanceSplits', 'PlayerProfile',
+                         'PlayerReboundLogTracking', 'PlayerReboundTracking', 'PlayerShootingSplits',
+                         'PlayerShotLogTracking', 'PlayerShotTracking', 'PlayerSummary', 'PlayerVsPlayer',
+                         'PlayerYearOverYearSplits', 'get_player']
+DOWNLOAD_STATS_GAME = ['Boxscore', 'BoxscoreAdvanced', 'BoxscoreFourFactors', 'BoxscoreMisc', 'BoxscoreScoring',
+                       'BoxscoreSummary', 'BoxscoreUsage', 'HustleStats', 'PlayByPlay', 'PlayerTracking']
+DOWNLOAD_STATS_TEAM = ['TeamClutchSplits', 'TeamCommonRoster', 'TeamDetail', 'TeamGameLogs', 'TeamGeneralSplits',
+                       'TeamInGameSplits', 'TeamLastNGamesSplits', 'TeamLineups', 'TeamList', 'TeamOpponentSplits',
+                       'TeamPassTracking', 'TeamPerformanceSplits', 'TeamPlayerOnOffDetail', 'TeamPlayers',
+                       'TeamReboundTracking', 'TeamSeasons', 'TeamShootingSplits', 'TeamShotTracking', 'TeamSummary',
+                       'TeamVsPlayer', 'TeamYearOverYearSplits']
---
+++ player_download_func.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from nba_py import player
+
+def check_file(file_name):
+    
+
+# gets career stats
+def career(player_id, per_mode='PerGame'):
+
